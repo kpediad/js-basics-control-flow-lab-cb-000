dae13d30ecb900f03ac107ea1d6cb697d5a222a2
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-19 15:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-
+  let response;
+  if (feet > 2500) {
+    response = "No can do.";
+  }
+  else if (feet > 2000 ) {
+    response = "I will gladly take your thirty bucks.";
+  } else if (feet <= 400) {
+    response = "This one is on me!";
+  }
+  return response;
 }
 
 function ternaryCheckCity(destination) {
